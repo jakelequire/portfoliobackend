d9fcb5db80784c3c9733dc3a30990de3eff23b54
Phock <jakelequire@gmail.com>
2023-02-08 17:40:07 -0800

    ###

--- server/dist/server.dev.js
+++ server/dist/server.dev.js
@@ -4,16 +4,26 @@ var express = require('express');
 
 var next = require('next');
 
+var cors = require('cors');
+
 var dev = process.env.NODE_ENV !== 'production';
 var app = next({
   dev: dev
 });
 var handle = app.getRequestHandler();
-var PORT = process.env.PORT || 3000;
+var PORT = 3001;
+var corsOptions = {
+  origin: 'http://localhost:3000/testdev',
+  credentials: true
+};
+cors(corsOptions);
 app.prepare().then(function () {
   var server = express();
-  server.get('*', function (req, res) {
+  server.get('/', function (req, res) {
     return handle(req, res);
+  });
+  server.get('/articles', function (req, res) {
+    res.send('Hello World!');
   });
   server.listen(PORT, function (err) {
     if (err) throw err;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const next = require('next');
+const cors = require('cors');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const PORT = process.env.PORT || 3000;
+const PORT = 3001;
+
+const corsOptions = {
+  origin: 'http://localhost:3000/testdev',
+  credentials: true,
+};
+
+cors(corsOptions);
 
 app.prepare().then(() => {
   const server = express();
 
-  server.get('*', (req, res) => {
+  server.get('/', (req, res) => {
     return handle(req, res);
+  });
+
+  server.get('/articles', (req, res) => {
+    res.send('Hello World!');
   });
 
   server.listen(PORT, err => {
