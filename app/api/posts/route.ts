c09be8c8e771c499db654445fadb4dbfffd38b89
Phock <jakelequire@gmail.com>
2023-03-09 22:03:17 -0800

    ###

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,4 +1,4 @@
-import type { RequestParams } from './../../../components/TypeDefinitions';
+import type { RequestParams } from '../../../components/TypeDefinitions';
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from 'next/server'
 import getArticles from "@/components/getArticles";
@@ -6,9 +6,14 @@ import Cors from 'cors'
 
 const cors = Cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'application/json'],
 })
 
 function middleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+  console.log("middleware called with req", req);
+  console.log("middleware called with res", res);
+  console.log("middleware called with fn", fn);
   return new Promise((resolve, reject) => {
     fn(req, res, (result: any) => {
       if (result instanceof Error) {
@@ -20,8 +25,9 @@ function middleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
   })
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<unknown>) {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
   console.log("<handler>")
+  console.log("<handler> - Method: ", req.method)
   await middleware(req, res, cors)
   if (req.method === "GET") {
     try {
@@ -31,30 +37,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
       console.log(typeof res, "<- RESPONSE");
 
-      return NextResponse.json(articles);
+      return res.json(articles);
     } catch (error) {
       throw new Error(error as string)
     }
   }
 }
-
-
-
-
-// export async function GET(
-//   Request: NextApiRequest,
-//   Response: NextApiResponse<unknown>
-// ) {
-//   console.log("<handler>")
-//   if (Request.method === "GET") {
-//     try {
-//       const queryParams = new URLSearchParams(Request.url);
-//       const query = queryParams.values().next().value;
-//       const articles = await getArticles(query);
-//       console.log(typeof Response, "<- RESPONSE");
-//       return NextResponse.json(articles);
-//     } catch (error) {
-//       throw new Error(error as string)
-//     }
-//   }
-// }
